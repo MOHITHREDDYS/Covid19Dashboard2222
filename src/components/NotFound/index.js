192f25921559ff560478560d786d174020c94be0
MOHITHREDDYS <mohithreddys.531@gmail.com>
2023-02-22 13:03:26 +0000

    code updated on 22 feb

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -9,12 +9,12 @@ const NotFound = props => {
 
   return (
     <div className="not-found-bg-container">
-      <img src={icon} alt="not found" className="not-found-image" />
+      <img src={icon} alt="not-found-pic" className="not-found-image" />
 
-      <h1 className="not-found-heading">Page Not Found</h1>
-      <h1 className="not-found-heading-1">PAGE NOT FOUND</h1>
+      {/* <h1 className="not-found-heading">Page Not Found</h1> */}
+      <h1 className="not-found-heading">PAGE NOT FOUND</h1>
       <p className="not-found-para">
-        we&apos;re sorry, the page you requested could not be found
+        we are sorry, the page you requested could not be found
       </p>
       <p className="not-found-para">Please go back to the homepage</p>
       <button
